9a60ca771619111d36b547ab8ce067c642296dc8
isom5240 <isom5240@users.noreply.huggingface.co>
2025-04-24 16:17:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import streamlit as st
 from transformers import pipeline
 
 # function part
-# img2text
-def img2text(url):
-    image_to_text_model = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
-    text = image_to_text_model(url)[0]["generated_text"]
-    return text
+# function part
+def generate_image_caption(image_path):
+    """Generates a caption for the given image using a pre-trained model."""
+    img2caption = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+    result = img2caption(image_path)
+    return result[0]['generated_text']
 
 # text2story
 def text2story(text):
@@ -15,30 +16,28 @@ def text2story(text):
     story_text = pipe(text)[0]['generated_text']
     return story_text
 
-# text2audio
-def text2audio(story_text):
-    pipe = pipeline("text-to-audio", model="Matthijs/mms-tts-eng")
-    audio_data = pipe(story_text)
-    return audio_data
+
 
 
 def main():
-    st.set_page_config(page_title="Your Image to Audio Story", page_icon="🦜")
-    st.header("Turn Your Image to Audio Story")
-    uploaded_file = st.file_uploader("Select an Image...")
+    # App title
+    st.title("Streamlit Demo on Hugging Face")
+
+    # Write some text
+    st.write("Welcome to a demo app showcasing basic Streamlit components!")
+    
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
         print(uploaded_file)
-        bytes_data = uploaded_file.getvalue()
-        with open(uploaded_file.name, "wb") as file:
-            file.write(bytes_data)
-        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+
+        st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
 
 
         #Stage 1: Image to Text
         st.text('Processing img2text...')
-        scenario = img2text(uploaded_file.name)
-        st.write(scenario)
+        image_caption = generate_image_caption(uploaded_image.name)
+        st.write(image_caption)
 
 if __name__ == "__main__":
     main()
