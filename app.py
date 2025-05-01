267ce0e7dd8a9d0d2914d6504c110222e7c76a21
isom5240 <isom5240@users.noreply.huggingface.co>
2025-05-01 07:30:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,35 @@
-# import part
 import streamlit as st
 from transformers import pipeline
-from PIL import Image
-import io
-
-# function part
-def generate_image_caption(image):
-    """Generates a caption for the given image using a pre-trained model."""
-    img2caption = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
-       
-    # Generate caption
-    result = img2caption(image)
-    return result[0]['generated_text']
-
-# text2story
-def text2story(text):
-    pipe = pipeline("text-generation", model="pranavpsv/genre-story-generator-v2")
-    story_text = pipe(text)[0]['generated_text']
-    return story_text
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+import torch
+import numpy as np
 
 def main():
-    # App title
-    st.title("Streamlit Demo on Hugging Face")
 
-    # Write some text
-    st.write("Welcome to a demo app showcasing basic Streamlit components!")
-    
-    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-    if uploaded_image is not None:
-        image = Image.open(uploaded_image).convert("RGB")
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.title("yelp2024fall Test")
+    st.write("Enter a sentence for analysis:")
 
-        # Stage 1: Image to Text
-        st.text('Processing img2text...')
-        image_caption = generate_image_caption(image)
-        st.write(image_caption)
+    user_input = st.text_input("")
+    if user_input:
+        # Approach: AutoModel
+        model2 = AutoModelForSequenceClassification.from_pretrained("isom5240/CustomModel_yelp2025L1",
+                                                                    num_labels=5)
+        tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+        inputs = tokenizer(user_input,
+                        padding=True,
+                        truncation=True,
+                        return_tensors='pt')
+
+        outputs = model2(**inputs)
+        predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+        predictions = predictions.cpu().detach().numpy()
+        # Get the index of the largest output value
+        max_index = np.argmax(predictions)
+        st.write(f"result (AutoModel) - Label: {max_index}")
+
 
 if __name__ == "__main__":
     main()
